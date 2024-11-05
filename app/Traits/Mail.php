d6a449a8c1f521e03ca9fbb67aadde394d390083
Neffer2 <n3f73r@gmail.com>
2024-11-05 13:59:13 -0500

    1. Correccipón Mailing

--- app/Traits/Mail.php
+++ app/Traits/Mail.php
@@ -25,7 +25,7 @@ trait Mail
             $mail->setFrom(env('MAIL_USERNAME'), 'Gana Como Loco');
             $mail->addAddress(auth()->user()->email, auth()->user()->name);
 
-            $mail->addAttachment(asset("assets/Mailing/$premio->mail"), "Descrubre tu premio.jpg");
+            $mail->addAttachment("public/assets/Mailing/{$premio->mail}", "Descrubre tu premio.jpg");
 
             //Content
             $mail->isHTML(true);
